aaa3e6413e35430b8a89a3ed86ff6f5a28ae8892
Jacky <newbdez33@gmail.com>
2020-06-27 07:51:10 +0900

    init

---
+++ pm2.js
@@ -0,0 +1,11 @@
+  var pm2 = require('pm2');
+
+  pm2.connect(function(err) {
+    if (err) throw err;
+
+  setTimeout(function worker() {
+    console.log("Restarting app...");
+    pm2.restart('ticker', function() {});
+    setTimeout(worker, 600000);
+    }, 600000);
+  });
